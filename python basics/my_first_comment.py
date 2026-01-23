9a1e112f1fa0f093d73b954ce4719400909c4b9d
Tytyrew <alisher.au08@gmail.com>
2026-01-23 13:44:09 +0500

    third commit

---
+++ python basics/my_first_comment.py
@@ -0,0 +1,8 @@
+#This is my comment
+
+"""
+Extendent version!
+hi hi hi ha
+"""
+print("Program with commets")
+# this is print function
---
+++ python basics/type.py
@@ -0,0 +1,9 @@
+
+x = 123012031203
+#this is type of variable x
+print(type(x))
+
+
+y = "joke"
+#this is type of variable y
+print(type(y))
