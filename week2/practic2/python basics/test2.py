c819c71e2ee1269bc6e76e9cc698771dab76495a
Tytyrew <alisher.au08@gmail.com>
2026-02-05 22:04:48 +0500

    [PP2]->week2_updateting_and_adding_example

---
+++ week2/practic2/python basics/test2.py
@@ -0,0 +1,9 @@
+for i1 in range(2):
+    for i2 in range(2):
+        for i3 in range(2):
+            for i4 in range(2):
+                for i5 in range(2):
+                    for i6 in range(2):
+                        for i7 in range(2):
+                            for i8 in range(2):
+                                print(i1,i2,i3,i4,i5,i6,i7,i8)
---
+++ week2/practic2/python basics/test5.py
@@ -0,0 +1,6 @@
+d = ['@']
+d.append(d)
+d.append(d)
+d.append(d)
+for i in range(len(d)):
+    print(d[i])
